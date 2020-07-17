0819afc47e8e2d4d813c56a1c8762cc01efeb0bc
Mariana Becerril <marianabecerril@M-Mac.local>
2020-07-17 12:00:56 -0700

    Operations in javascript.

---
+++ Assignments/Week_5/Javascript/scripts/main.js
@@ -0,0 +1,20 @@
+// Math operators in Javascript.
+// + - / * %
+
+var number1 =10;
+var number2 = 3;
+
+// addition
+var sum = number1 + number2;
+
+//Substraction
+var difference = number1 - number2
+
+//Multiplication
+var product = number1 * number2
+
+//division
+var quotient = number1 / number2
+
+//modulo or remainder
+var modulus = number1 % number2;
