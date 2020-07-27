e6c90796bfc40ef3b055af7c6bac1cb79cc7972d
Mariana Becerril <marianabecerril@M-Mac.local>
2020-07-27 12:37:32 -0700

    Objects

---
+++ Assignments/Week_6/Javascript/scripts/main objects.js
@@ -0,0 +1,26 @@
+var myObject = [
+    "Eduardo", "Garcia", 30, "Burritos"
+];
+
+var myObject = {
+    "firstName": "Eduardo",
+    "lastName": "Garcia",
+    "age": 30,
+    "favFood": "Burrito",
+    "goToWork": function () {
+        console.log("goes to work");
+    }
+};
+
+// Square bracket notation
+console.log(myObject["firstName"] + "'s age is " + myObject ["age"]);
+
+myObject["age"] = 31;
+myObject["age"] = myObject["age"] + 1;
+
+// Dot notation
+myObject.age;
+
+//myObject.goToWork();
+asteriskString.length;
+asteriskString.split();
